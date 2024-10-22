a7681591cbeae4b3a13da8fad93e221a5862e16c
alfarezyyd <adityaalfarezyd@gmail.com>
2024-10-23 00:22:08 +0700

    feat: demo example form

---
+++ src/main/java/alfarezyyd/eigthMeeting/ExampleForm.java
@@ -0,0 +1,9 @@
+package alfarezyyd.eigthMeeting;
+
+import javax.swing.*;
+
+public class ExampleForm {
+  private JCheckBox checkBox1;
+  private JCheckBox checkBox2;
+  private JButton button1;
+}
